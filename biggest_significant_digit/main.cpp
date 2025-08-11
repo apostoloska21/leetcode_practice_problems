85e46b5ba9d1c15c3b9175236d0d6038c73b754f
Martina <apostoloska21@gmail.com>
2025-08-11 14:11:53 +0200

    update: added solution to summing elements with pointers

--- biggest_significant_digit/main.cpp
+++ biggest_significant_digit/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int mostSignificantDigit(int n){
+    string s = to_string(n);
+    return s[0]-'0';
+}
 
 int main() {
-    
+    int n;
+    while(cin>>n && n!=0){
+        vector<int>numbers(n);
+        for(int i=0; i<n; i++){
+            cin>>numbers[i];
+        }
+        int maxDigit= mostSignificantDigit(numbers[0]);
+        for(int i=0; i<n; i++){
+            if(mostSignificantDigit(numbers[i]) > maxDigit){
+                maxDigit=mostSignificantDigit(numbers[i]);
+            }
+        }
+        int result = numbers[0];
+        for(int i=0; i<n; i++){
+            if(mostSignificantDigit(numbers[i]) == maxDigit){
+                result=numbers[i];
+                break;
+            }
+        }
+        cout<<result<<endl;
+    }
+
     return 0;
 }
---
+++ sum_elements_pointers/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int sum_pos(int *arr, int ind, int n){
+    if(ind>=n) return 0;
+    int sum=0;
+    for(int i=ind; i<n; i++)
+        sum+=*(arr+i);
+    return sum;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int ind;
+    cin>>ind;
+    cout<<sum_pos(arr, ind, n)<<endl;
+    return 0;
+}
