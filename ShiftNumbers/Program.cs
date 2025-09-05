6811e66b12b4aceb3a6051a2529648a29bb72bd1
Martina <apostoloska21@gmail.com>
2025-09-05 16:28:15 +0200

    implemented program to shift negative numbers to the right

---
+++ ShiftNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Premesti(int[] a, int n)
+    {
+        int[] rezultat = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= 0)
+            {
+                rezultat[index] = a[i];
+                index++;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] < 0)
+            {
+                rezultat[index] = a[i];
+                index++;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = rezultat[i];
+        }
+    }
+
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());  
+        int[] a = new int[N];
+
+        string[] parts = Console.ReadLine().Split(' ');
+        for (int i = 0; i < N; i++)
+        {
+            a[i] = int.Parse(parts[i]);
+        }
+
+        Premesti(a, N);
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write(a[i]);
+            if (i < N - 1) Console.Write(" ");
+        }
+    }
+}
